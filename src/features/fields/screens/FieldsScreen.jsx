fcb204f89324817666c5b367667320019fd7a862
DAMB1808 <dmauricio-2022464@kinal.edu.gt>
2026-06-16 08:21:38 -0600

    se realizaron modificaciones

---
+++ src/features/fields/screens/FieldsScreen.jsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+
+const FieldsScreen = () => {
+  return (
+    <View>
+        <Text>FieldsScreen</Text>
+    </View>
+    )
+}
+
+export default FieldsScreen;
--- src/navigation/AppNavigator.jsx
+++ src/navigation/AppNavigator.jsx
@@ -1,12 +1,26 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { View, ActivityIndicator, StyleSheet } from "react-native";
 import { COLORS } from "../shared/constants/theme";
-import AuthStackNavigator from "./AuthStack";
+import AuthStack from "./AuthStack";
+import MainTabs from "./MainTabs";
+import { useAuthStore } from "../shared/store/authStore";
 
 const AppNavigator = () => {
+
+    const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
+    const isHydrated = useAuthStore((state) => state._hasHydrated);
+
+    if (!isHydrated) {
+        return (
+            <View>
+                <ActivityIndicator size="large" color={COLORS.primary} />
+            </View>
+        );
+    }
+
     return (
         <NavigationContainer>
-            <AuthStackNavigator />
+            { isAuthenticated ? <MainTabs /> : <AuthStack /> }
         </NavigationContainer>
     );
 };
